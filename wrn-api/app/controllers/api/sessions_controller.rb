ccad02e3b3d97ad86aa2fd750bb4a741c0487434
yianyue <yianyue@gmail.com>
2015-08-21 16:09:14 -0400

    TODO: reload with new user info OR logout

--- wrn-api/app/controllers/api/sessions_controller.rb
+++ wrn-api/app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       @user.generate_token
-      render json: @user.as_json(only: [:name, :email, :token]), status: 200
+      render json: @user.as_json(only: [:name, :email, :goal, :token]), status: 200
     else
       # TODO: more detail error
       render json: {error: 'invalid login'}, status: 401
